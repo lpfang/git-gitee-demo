c5a4ead5d2f3b7e93e7ed119c78d19f2ba8875c4
lpfang <flp@zjut.edu.cn>
2025-06-11 11:34:09 +0800

    add third line to

--- main.cpp
+++ main.cpp
@@ -4,5 +4,6 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "This project uses the C++14 language standard version." << std::endl;
     std::cout << "Enjoy coding!" << std::endl;
+    std::cout << "Goodbye!" << std::endl;
     return 0;
 }
