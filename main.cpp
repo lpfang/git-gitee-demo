ff60202e2e8b21b3f494d70ad4d30eb3465d214c
lpfang <flp@zjut.edu.cn>
2025-06-11 12:40:02 +0800

    add forth commit to main.cpp

--- main.cpp
+++ main.cpp
@@ -5,5 +5,6 @@ int main() {
     std::cout << "This project uses the C++14 language standard version." << std::endl;
     std::cout << "Enjoy coding!" << std::endl;
     std::cout << "Goodbye!" << std::endl;
+    std::cout << "End of program." << std::endl;
     return 0;
 }
